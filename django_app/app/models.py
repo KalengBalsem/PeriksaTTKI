ec3f574631f661cb87fd026cbbb4aab343b6695e
KalengBalsem <balsemcode@gmail.com>
2024-06-18 20:45:26 +0700

    working user_correction input + db model

--- django_app/app/models.py
+++ django_app/app/models.py
@@ -7,3 +7,9 @@ class Chat(models.Model):
     message = models.TextField()
     response = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+class UserCorrection(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    original_word = models.TextField()
+    user_correction = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
--- django_app/app/views.py
+++ django_app/app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django_ratelimit.decorators import ratelimit
-from .models import Chat
+from .models import Chat, UserCorrection
 
 from django.utils import timezone
-
+import json
 # inferencing the model
 from .model_inference import call_model
 ####
@@ -24,22 +24,28 @@ def index(request):
 @ratelimit(key='user_or_ip', rate='5/m', method=['POST'], block=False)
 def main(request):
     if request.method == 'POST':
-        user_input = request.POST.get('user_input')
-        response = call_model(user_input)
-        typo_words = response['typo_words']  # typo words is not used..
-        error_words = response['error_words']
-        paraphrase = response['paraphrase']
-
-        if getattr(request, 'limited', False):
-            return JsonResponse({'typo_words': 'Terlalu Banyak Request (max=5/minute).', 'error_words': '', 'paraphrase': 'Terlalu Banyak Request (max=5/minute).'})
-
-        # registering user input to the database
-        chat = Chat(user=request.user, message=user_input, response=response, timestamp=timezone.now())
-        chat.save()
-        ####
-
-        return JsonResponse({'typo_words': typo_words, 'error_words': error_words, 'paraphrase': paraphrase})
-    
+        if request.POST.get('data_type') == 'user_input':
+            user_input = request.POST.get('user_input')
+            response = call_model(user_input)
+            typo_words = response['typo_words']  # typo words is not used..
+            error_words = response['error_words']
+            paraphrase = response['paraphrase']
+            if getattr(request, 'limited', False):
+                return JsonResponse({'typo_words': 'Terlalu Banyak Request (max=5/minute).', 'error_words': '', 'paraphrase': 'Terlalu Banyak Request (max=5/minute).'})
+            # registering user input to the database
+            chat = Chat(user=request.user, message=user_input, response=response, timestamp=timezone.now())
+            chat.save()
+            ####
+            return JsonResponse({'typo_words': typo_words, 'error_words': error_words, 'paraphrase': paraphrase})
+        elif request.POST.get('data_type') == 'user_correction':
+            user_correction_data = json.loads(request.POST.get('user_correction'))
+            original_word = user_correction_data['original_word']
+            user_correction = user_correction_data['user_correction']
+            # registering user_correction to the database
+            usercorrection = UserCorrection(user=request.user, original_word=original_word, user_correction=user_correction, timestamp=timezone.now())
+            usercorrection.save()
+            ####
+            return HttpResponse(json.dumps('user_correction has successfully stored.'))
     elif request.user.is_authenticated:
         return render(request, 'main.html')
     else:
